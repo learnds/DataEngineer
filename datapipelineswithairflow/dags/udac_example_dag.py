07ff761ae6804572a8cc60f91d9fb356aa85dc3f
Chethan T <chethan_t@yahoo.com>
2021-03-03 23:01:23 -0500

    Implemented stage_redshift task

--- datapipelineswithairflow/dags/udac_example_dag.py
+++ datapipelineswithairflow/dags/udac_example_dag.py
@@ -11,26 +11,48 @@ from helpers import SqlQueries
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
-}
+    'start_date': datetime(2018,11,1),
+    'end_date': datetime(2018,11,5),
+    'retries': 1,
+    'depends_on_past': False,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False,
+    'email_on_retry': False
+    }
 
 dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
+          schedule_interval='0 0 * * *',
+          
         )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
     task_id='Stage_events',
-    dag=dag
+    dag=dag,
+    table="staging_events",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend/",
+    s3_key="log_data",
+    region="us-west-2",
+    jsonpath="s3://udacity-dend/log_json_path.json"
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
     task_id='Stage_songs',
-    dag=dag
+    dag=dag,
+    table="staging_songs",
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    s3_bucket="udacity-dend/",
+    s3_key="song_data",
+    region="us-west-2",
+    jsonpath="auto"
 )
+
 
 load_songplays_table = LoadFactOperator(
     task_id='Load_songplays_fact_table',
@@ -62,4 +84,23 @@ run_quality_checks = DataQualityOperator(
     dag=dag
 )
 
+
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+load_user_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+run_quality_checks >> end_operator
+
+
+
+
--- datapipelineswithairflow/plugins/operators/stage_redshift.py
+++ datapipelineswithairflow/plugins/operators/stage_redshift.py
@@ -1,24 +1,73 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    template_fields = ("s3_key",)
+    copy_sql = """
+            COPY {}
+            FROM '{}'
+            ACCESS_KEY_ID '{}'
+            SECRET_ACCESS_KEY '{}'
+            REGION '{}'
+            JSON '{}'
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id = "",
+                 table="",
+                 s3_bucket = "",
+                 s3_key = "",
+                 region = "",
+                 jsonpath = "",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.conn_id = redshift_conn_id
+        self.table = table
+        self.aws_credentials_id = aws_credentials_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.region = region
+        self.jsonpath = jsonpath
+        
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        self.log.info('Loading staging table')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id = self.conn_id)
+        
+#        self.log.info('Deleting staging table')
+#        redshift.run("DELETE FROM {}".format(self.table))
+        
+        self.log.info("Copying from S3 to redshift")
+        rend_key = self.s3_key.format(**context)
+        s3_path = "s3://{}{}".format(self.s3_bucket,rend_key)
+       
+        final_copy_sql = StageToRedshiftOperator.copy_sql.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.region,
+            self.jsonpath
+        )
+        redshift.run(final_copy_sql)
+
+        
+       
+        
+        
+        
+        
+        
+        
+        
 
 
 

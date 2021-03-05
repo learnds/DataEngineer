d2d8a39dc71005f4397b83a504364f69d53afbdb
Chethan T <chethan_t@yahoo.com>
2021-03-04 21:57:39 -0500

    Passing table query directly from main dag

--- datapipelineswithairflow/dags/udac_example_dag.py
+++ datapipelineswithairflow/dags/udac_example_dag.py
@@ -65,28 +65,28 @@ load_user_dimension_table = LoadDimensionOperator(
     task_id='Load_user_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    table="user"
+    table_query=SqlQueries.user_table_insert
 )
 
 load_song_dimension_table = LoadDimensionOperator(
     task_id='Load_song_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    table="song"
+    table_query=SqlQueries.song_table_insert
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
     task_id='Load_artist_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    table="artist"
+    table_query=SqlQueries.artist_table_insert
 )
 
 load_time_dimension_table = LoadDimensionOperator(
     task_id='Load_time_dim_table',
     dag=dag,
     redshift_conn_id="redshift",
-    table="time"
+    table_query=SqlQueries.time_table_insert
 )
 
 run_quality_checks = DataQualityOperator(
--- datapipelineswithairflow/plugins/operators/load_dimension.py
+++ datapipelineswithairflow/plugins/operators/load_dimension.py
@@ -1,7 +1,7 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
-from helpers import SqlQueries
+
 
 class LoadDimensionOperator(BaseOperator):
 
@@ -10,30 +10,17 @@ class LoadDimensionOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 table="",
+                 table_query="",
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
         self.conn_id = redshift_conn_id
-        self.table = table
+        self.table_query = table_query
 
     def execute(self, context):
-        self.log.info("Loading dimenstion table " + self.table )
+        self.log.info("Loading dimenstion table -" + self.table_query)
         redshift = PostgresHook(postgres_conn_id = self.conn_id)
                       
-                
-#        self.log.info('Deleting staging table')
-#        redshift.run("DELETE FROM {}".format(self.table))
-                      
-        if self.table == "time":
-            load_dim_sql = SqlQueries.time_table_insert
-        elif self.table == "artist":
-            load_dim_sql = SqlQueries.artist_table_insert
-        elif self.table == "song":
-            load_dim_sql = SqlQueries.song_table_insert
-        elif self.table == "user":
-            load_dim_sql = SqlQueries.user_table_insert
-        
-        self.log.info(load_dim_sql)
+        load_dim_sql = self.table_query
         redshift.run(load_dim_sql)
-        self.log.info("Done loading dim table " + self.table)
+        self.log.info("Done loading dim table ")
